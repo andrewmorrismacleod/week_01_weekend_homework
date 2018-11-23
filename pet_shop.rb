0862f95b3197abe8c40ca3162de911d82f78c805
Andrew MacLeod <andrewmorrismacleod@googlemail.com>
2018-11-23 17:11:13 +0000

    Adding Test Optional 1

--- pet_shop.rb
+++ pet_shop.rb
@@ -57,3 +57,19 @@ end
 def customer_can_afford_pet(customer, new_pet)
   return customer[:cash] >= new_pet[:price]
 end
+
+def sell_pet_to_customer(pet_shop, pet, customer)
+
+  if customer_can_afford_pet(customer, pet)
+
+    add_pet_to_customer(customer, pet)
+    remove_pet_by_name(pet_shop, pet[:name])
+    increase_pets_sold(pet_shop, 1)
+    remove_customer_cash(customer, pet[:price])
+    add_or_remove_cash(pet_shop,pet[:price])
+
+  else
+
+  end
+
+end
